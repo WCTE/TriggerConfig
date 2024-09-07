6e024010a80d9a8cbcdcdfcc1eb592d7aba298fe
Dean Karlen <karlen@uvic.ca>
2024-09-06 17:15:44 -0700

    Define the dictionaries and start initialization

---
+++ Configuration.py
@@ -0,0 +1,345 @@
+# Define the configuration of the trigger through nested dictionaries that can be written to a JSON file
+
+# Import the necessary modules
+import json
+
+# Define the configuration class
+class Configuration:
+    # Define the constructor
+    def __init__(self, short_name: str, description: str):
+        # Define the configuration dictionary
+        self.configuration = {
+            "short name": short_name,
+            "description": description,
+            "version": "1.0",
+            "input signals": {},
+            "input signal treatment": {},
+            "level 1 logics": {},
+            "level 1 output treatment": {},
+            "level 2 logics": {},
+            "level 2 output treatment": {},
+            "output lemo assignments": {},
+            "prescalers": {},
+            "deadtime veto": {}
+        }
+        self.maximum_delay = 255
+        self.maximum_window_length = 255
+
+    def check_serial(self, serial: int, min_value: int, max_value: int):
+        # check that the serial number is a number between the min and max values
+        if not (isinstance(serial, int) and min_value <= serial <= max_value):
+            print(f'invalid serial number {serial}. It must be a number between {min_value} and {max_value}')
+            return False
+        return True
+
+    def check_string(self, string: str, max_length: int):
+        # check that the string is a string of length at most max_length
+        if not isinstance(string, str) or len(string) > max_length:
+            print(f'invalid string {string}. It must be a string of length at most {max_length}')
+            return False
+        return True
+
+    def check_bool(self, boolean: bool):
+        # check that the boolean is a boolean
+        if not isinstance(boolean, bool):
+            print(f'invalid boolean {boolean}. It must be a boolean')
+            return False
+        return True
+
+    # Define a new signal and add to the trigger configuration
+    def set_signal(self, serial: int, short_name: str, description: str, verbose: bool = True):
+
+        fail = False
+        # check that the serial number is a number between 0 and 95
+        fail = fail or not self.check_serial(serial, 0, 95)
+
+        # check that the short name is a string of length at most 10 (for printing purposes)
+        fail = fail or not self.check_string(short_name, 10)
+
+        # check that the description is a string of length at most 60 (for printing purposes)
+        fail = fail or not self.check_string(description, 60)
+
+        if not fail:
+
+            # check if the serial number is already in the configuration
+            new_signal = True
+            if serial in self.configuration["input signals"]:
+                new_signal = False
+                short = self.configuration["input signals"][serial]["short name"]
+                if verbose:
+                    print(f'signal {serial} ({short}) replaced by new signal {short_name}')
+                return
+
+            # Assign the signal dictionary to the configuration
+            signal = {
+                "short name": short_name,
+                "description": description
+            }
+            self.configuration["input signals"][serial] = signal
+            if verbose:
+                print(f'signal {serial} ({short_name}) added')
+
+            # Set the default treatment for new signals:
+            if new_signal:
+                self.set_treatment(serial, verbose=verbose)
+                if verbose:
+                    print(f'default treatment for signal {serial} set')
+
+    def set_treatment(self, serial: int, delay: int = 0, window_length: int = 1, invert: bool = False, verbose: bool = True):
+
+        fail = False
+        # check that the serial number is a number between 0 and 95
+        fail = fail or not self.check_serial(serial, 0, 95)
+
+        fail = fail or not self.check_serial(delay, 0, self.maximum_delay)
+        fail = fail or not self.check_serial(window_length, 1, self.maximum_window_length)
+
+        # check that the invert is a boolean
+        fail = fail or not self.check_bool(invert)
+
+        if not fail:
+            # Define the treatment dictionary
+            treatment = {
+                "delay": delay,
+                "window length": window_length,
+                "invert": invert
+            }
+
+            # Assign the treatment dictionary to the configuration
+            self.configuration["input signal treatment"][serial] = treatment
+            if verbose:
+                print(f'treatment for signal {serial} set')
+
+    def set_level_1_logic(self, serial:int, short_name:str, description: str, inputs: list, logic_type: str, verbose: bool = True):
+        fail = False
+        # check that the serial number is a number between 0 and 9
+        fail = fail or not self.check_serial(serial, 0, 9)
+
+        # check that the short name is a string of length at most 10 (for printing purposes)
+        fail = fail or not self.check_string(short_name, 10)
+
+        # check that the description is a string of length at most 60 (for printing purposes)
+        fail = fail or not self.check_string(description, 60)
+
+        # check that the inputs is a list of numbers between 0 and 63
+        if not all(isinstance(i, int) and 0 <= i <= 63 for i in inputs):
+            fail = True
+            print(f'invalid inputs {inputs}. It must be a list of numbers between 0 and 63')
+
+        # check that the logic type is a string and either "and" or "or"
+        if not isinstance(logic_type, str) or logic_type not in ["AND", "OR"]:
+            fail = True
+            print(f'invalid logic type {logic_type}. It must be a string and either "AND" or "OR"')
+
+        if not fail:
+
+            # check if the serial number is already in the configuration
+            new_logic = True
+            if serial in self.configuration["level 1 logics"]:
+                new_logic = False
+                short = self.configuration["level 1 logics"][serial]["short name"]
+                if verbose:
+                    print(f'level 1 logic {serial} ({short}) replaced by new level 1 logic {short_name}')
+                return
+
+            # Define the logic dictionary
+            logic = {
+                "short name": short_name,
+                "description": description,
+                "inputs": inputs,
+                "type": logic_type
+            }
+            # Assign the logic dictionary to the configuration
+            self.configuration["level 1 logics"][serial] = logic
+            if verbose:
+                print(f'level 1 logic {serial} ({short_name}) added')
+
+            # Set the default treatment for new level 1 logics:
+            if new_logic:
+                self.set_level_1_treatment(serial, verbose=verbose)
+                if verbose:
+                    print(f'default treatment for level 1 logic output {serial} set')
+
+
+    def set_level_1_treatment(self, serial: int, delay: int = 0, window_length: int = 1, invert: bool = False, verbose: bool = True):
+
+        fail = False
+        # check that the serial number is a number between 0 and 9
+        fail = fail or not self.check_serial(serial, 0, 9)
+
+        fail = fail or not self.check_serial(delay, 0, self.maximum_delay)
+        fail = fail or not self.check_serial(window_length, 0, self.maximum_window_length)
+
+        # check that the invert is a boolean
+        fail = fail or not self.check_bool(invert)
+
+        if not fail:
+            # Define the treatment dictionary
+            treatment = {
+                "delay": delay,
+                "window length": window_length,
+                "invert": invert
+            }
+
+            # Assign the treatment dictionary to the configuration
+            self.configuration["level 1 output treatment"][serial] = treatment
+            if verbose:
+                print(f'treatment for level 1 output {serial} set')
+
+
+    def set_level_2_logic(self, serial:int, short_name:str, description: str, inputs: list, level_1_inputs: list, logic_type: str, verbose: bool = True):
+        fail = False
+        # check that the serial number is a number between 0 and 3
+        fail = fail or not self.check_serial(serial, 0, 3)
+
+        # check that the short name is a string of length at most 10 (for printing purposes)
+        fail = fail or not self.check_string(short_name, 10)
+
+        # check that the description is a string of length at most 60 (for printing purposes)
+        fail = fail or not self.check_string(description, 60)
+
+        # check that the inputs is a list of numbers between 0 and 63
+        if not all(isinstance(i, int) and 0 <= i <= 63 for i in inputs):
+            fail = True
+            print(f'invalid inputs {inputs}. It must be a list of numbers between 0 and 63')
+
+        # check that the level_1_inputs is a list of numbers between 0 and 9
+        if not all(isinstance(i, int) and 0 <= i <= 9 for i in level_1_inputs):
+            fail = True
+            print(f'invalid inputs {inputs}. It must be a list of numbers between 0 and 9')
+
+        # check that the logic type is a string and either "and" or "or"
+        if not isinstance(logic_type, str) or logic_type not in ["AND", "OR"]:
+            fail = True
+            print(f'invalid logic type {logic_type}. It must be a string and either "AND" or "OR    "')
+
+        if not fail:
+
+            # check if the serial number is already in the configuration
+            new_logic = True
+            if serial in self.configuration["level 2 logics"]:
+                new_logic = False
+                short = self.configuration["level 2 logics"][serial]["short name"]
+                if verbose:
+                    print(f'level 2 logic {serial} ({short}) replaced by new level 2 logic {short_name}')
+                return
+
+            # Define the logic dictionary
+            logic = {
+                "short name": short_name,
+                "description": description,
+                "inputs": inputs,
+                "level 1 inputs": level_1_inputs,
+                "type": logic_type
+            }
+            # Assign the logic dictionary to the configuration
+            self.configuration["level 2 logics"][serial] = logic
+            if verbose:
+                print(f'level 1 logic {serial} ({short_name}) added')
+
+            # Set the default treatment for new level 2 logics:
+            if new_logic:
+                self.set_level_2_treatment(serial, verbose=verbose)
+                if verbose:
+                    print(f'default treatment for level 2 logic output {serial} set')
+
+    def set_level_2_treatment(self, serial: int, delay: int = 0, window_length: int = 1, invert: bool = False, verbose: bool = True):
+
+        fail = False
+        # check that the serial number is a number between 0 and 3
+        fail = fail or not self.check_serial(serial, 0, 3)
+
+        fail = fail or not self.check_serial(delay, 0, self.maximum_delay)
+        fail = fail or not self.check_serial(window_length, 0, self.maximum_window_length)
+
+        # check that the invert is a boolean
+        fail = fail or not self.check_bool(invert)
+
+        if not fail:
+            # Define the treatment dictionary
+            treatment = {
+                "delay": delay,
+                "window length": window_length,
+                "invert": invert
+            }
+
+            # Assign the treatment dictionary to the configuration
+            self.configuration["level 2 output treatment"][serial] = treatment
+            if verbose:
+                print(f'treatment for level 2 output {serial} set')
+
+    def set_output_lemo_assignment(self, serial: int, source: str, source_serial: int, treatment: bool, verbose: bool = True):
+        fail = False
+        # check that the serial number (output lemo connector number) is a number between 0 and 15
+        fail = fail or not self.check_serial(serial, 0, 15)
+
+        # check that the source is one of "input", "level 1", "level 2"
+        if source not in ["input", "level 1", "level 2"]:
+            fail = True
+            print(f'invalid source {source}. It must be one of "input", "level 1", "level 2"')
+
+        # check that the source serial number is valid
+        if source == "input":
+            fail = fail or not self.check_serial(source_serial, 0, 95)
+        elif source == "level 1":
+            fail = fail or not self.check_serial(source_serial, 0, 9)
+        elif source == "level 2":
+            fail = fail or not self.check_serial(source_serial, 0, 3)
+
+        # check that the treatment is a boolean (specifies if the treatment is applied or not)
+        fail = fail or not self.check_bool(treatment)
+
+        if not fail:
+            # Define the output dictionary
+            output = {
+                "source": source,
+                "source serial": source_serial,
+                "treatment": treatment
+            }
+            # Assign the output to the configuration
+            self.configuration["output lemo assignments"][serial] = output
+            if verbose:
+                print(f'output {serial} assigned to LEMO {output}')
+
+    def set_prescaler(self, serial: int, prescaler: int, verbose: bool = True):
+        fail = False
+        # check that the serial number is a number between 0 and 2
+        fail = fail or not self.check_serial(serial, 0, 2)
+
+        # check that the prescaler is a number between 0 and 8
+        fail = fail or not self.check_serial(prescaler, 0, 8)
+
+        if not fail:
+            # Assign the prescaler to the configuration
+            self.configuration["prescalers"][serial] = prescaler
+            if verbose:
+                print(f'prescaler {serial} set to {prescaler}')
+
+    def set_deadtime_veto(self, deadtime, verbose: bool = True):
+        fail = False
+        # check that the deadtime is a number between 1 and 500 (in us)
+        fail = fail or not self.check_serial(deadtime, 0, 500)
+
+        if not fail:
+            self.configuration["deadtime veto"] = deadtime
+            if verbose:
+                print(f'deadtime veto set')
+
+    # Define the get configuration method
+    def get_configuration(self):
+        # Return the configuration
+        return self.configuration
+
+    # Define the write configuration method
+    def write_configuration(self, filename):
+        # Open the file for writing
+        with open(filename, "w") as file:
+            # Write the configuration to the file
+            json.dump(self.configuration, file, indent=4)
+
+    # Define the read configuration method
+    def read_configuration(self, filename):
+        # Open the file for reading
+        with open(filename, "r") as file:
+            # Read the configuration from the file
+            self.configuration = json.load(file)
---
+++ initialization.py
@@ -0,0 +1,139 @@
+# Create a trigger configuration from scratch
+
+# Import the necessary modules
+from Configuration import Configuration
+
+# Define the default signal channel assignments
+def set_signal_channels(c, verbose = False):
+
+    # Trigger scintillator 0
+    c.set_signal(0, "T00", "Upstream trigger scintillator 0 channel 0", verbose)
+    c.set_signal(1, "T01", "Upstream trigger scintillator 0 channel 1", verbose)
+    c.set_signal(2, "T02", "Upstream trigger scintillator 0 channel 2", verbose)
+    c.set_signal(3, "T03", "Upstream trigger scintillator 0 channel 3", verbose)
+
+    # Trigger scintillator 1
+    c.set_signal(4, "T10", "Downstream trigger scintillator 1 channel 0", verbose)
+    c.set_signal(5, "T11", "Downstream trigger scintillator 1 channel 1", verbose)
+    c.set_signal(6, "T12", "Downstream trigger scintillator 1 channel 2", verbose)
+    c.set_signal(7, "T13", "Downstream trigger scintillator 1 channel 3", verbose)
+    c.set_signal(8, "T2", "Small trigger scintillator upstream of magnet", verbose)
+
+    # Hole counters
+    c.set_signal(9, "HC0", "Hole counter 0", verbose)
+    c.set_signal(10, "HC1", "Hole counter 1", verbose)
+    c.set_signal(11, "HC2", "Hole counter 2", verbose)
+
+    # ACT modules
+    c.set_signal(12, "ACT00", "ACT module 0 channel 0", verbose)
+    c.set_signal(13, "ACT01", "ACT module 0 channel 1", verbose)
+    c.set_signal(14, "ACT10", "ACT module 1 channel 0", verbose)
+    c.set_signal(15, "ACT11", "ACT module 1 channel 1", verbose)
+    c.set_signal(16, "ACT20", "ACT module 2 channel 0", verbose)
+    c.set_signal(17, "ACT21", "ACT module 2 channel 1", verbose)
+    c.set_signal(18, "ACT30", "ACT module 3 channel 0", verbose)
+    c.set_signal(19, "ACT31", "ACT module 3 channel 1", verbose)
+    c.set_signal(20, "ACT40", "ACT module 4 channel 0", verbose)
+    c.set_signal(21, "ACT41", "ACT module 4 channel 1", verbose)
+    c.set_signal(22, "ACT50", "ACT module 5 channel 0", verbose)
+    c.set_signal(23, "ACT51", "ACT module 5 channel 1", verbose)
+
+    # Muon tagger
+    c.set_signal(24, "MUT0", "Muon tagger channel 0", verbose)
+    c.set_signal(25, "MUT1", "Muon tagger channel 1", verbose)
+
+    # spill information
+    c.set_signal(26, "BSW", "Beam spill warning", verbose)
+    c.set_signal(27, "BSE", "Beam spill end", verbose)
+
+    # other trigger inputs
+    c.set_signal(28, "CALIB", "Laser or other asynchronous trigger signal", verbose)
+    c.set_signal(29, "TOF0F", "TOF module 0 channel 15", verbose)
+
+    # 1 TOF channel left over
+    c.set_signal(30, "TOF0F", "TOF module 0 channel 15", verbose)
+    c.set_signal(31, "TDCT0", "TDC stop signal for TDC0", verbose)
+
+    # Hodoscope modules: 15 channels labelled as hexidecimal
+    c.set_signal(32, "HODO0", "Hodoscope channel 0", verbose)
+    c.set_signal(33, "HODO1", "Hodoscope channel 1", verbose)
+    c.set_signal(34, "HODO2", "Hodoscope channel 2", verbose)
+    c.set_signal(35, "HODO3", "Hodoscope channel 3", verbose)
+    c.set_signal(36, "HODO4", "Hodoscope channel 4", verbose)
+    c.set_signal(37, "HODO5", "Hodoscope channel 5", verbose)
+    c.set_signal(38, "HODO6", "Hodoscope channel 6", verbose)
+    c.set_signal(39, "HODO7", "Hodoscope channel 7", verbose)
+    c.set_signal(40, "HODO8", "Hodoscope channel 8", verbose)
+    c.set_signal(41, "HODO9", "Hodoscope channel 9", verbose)
+    c.set_signal(42, "HODOA", "Hodoscope channel 10", verbose)
+    c.set_signal(43, "HODOB", "Hodoscope channel 11", verbose)
+    c.set_signal(44, "HODOC", "Hodoscope channel 12", verbose)
+    c.set_signal(45, "HODOD", "Hodoscope channel 13", verbose)
+    c.set_signal(46, "HODOE", "Hodoscope channel 14", verbose)
+    c.set_signal(47, "HODOF", "Hodoscope channel 15", verbose)
+    c.set_signal(48, "TDCT0", "TDC stop signal for TDC1", verbose)
+
+# Channels 49-63 are not used since we have only 5 discriminator modules (80 channels)
+
+    # TOF modules - 16 channels, labelled as hexidecimal
+    c.set_signal(64, "TOF00", "TOF module 0 channel 0", verbose)
+    c.set_signal(65, "TOF01", "TOF module 0 channel 1", verbose)
+    c.set_signal(66, "TOF02", "TOF module 0 channel 2", verbose)
+    c.set_signal(67, "TOF03", "TOF module 0 channel 3", verbose)
+    c.set_signal(68, "TOF04", "TOF module 0 channel 4", verbose)
+    c.set_signal(69, "TOF05", "TOF module 0 channel 5", verbose)
+    c.set_signal(70, "TOF06", "TOF module 0 channel 6", verbose)
+    c.set_signal(71, "TOF07", "TOF module 0 channel 7", verbose)
+    c.set_signal(72, "TOF08", "TOF module 0 channel 8", verbose)
+    c.set_signal(73, "TOF09", "TOF module 0 channel 9", verbose)
+    c.set_signal(74, "TOF0A", "TOF module 0 channel 10", verbose)
+    c.set_signal(75, "TOF0B", "TOF module 0 channel 11", verbose)
+    c.set_signal(76, "TOF0C", "TOF module 0 channel 12", verbose)
+    c.set_signal(77, "TOF0D", "TOF module 0 channel 13", verbose)
+    c.set_signal(78, "TOF0E", "TOF module 0 channel 14", verbose)
+    c.set_signal(79, "TOF0F", "TOF module 0 channel 15", verbose)
+
+    c.set_signal(80, "TOF10", "TOF module 1 channel 0", verbose)
+    c.set_signal(81, "TOF11", "TOF module 1 channel 1", verbose)
+    c.set_signal(82, "TOF12", "TOF module 1 channel 2", verbose)
+    c.set_signal(83, "TOF13", "TOF module 1 channel 3", verbose)
+    c.set_signal(84, "TOF14", "TOF module 1 channel 4", verbose)
+    c.set_signal(85, "TOF15", "TOF module 1 channel 5", verbose)
+    c.set_signal(86, "TOF16", "TOF module 1 channel 6", verbose)
+    c.set_signal(87, "TOF17", "TOF module 1 channel 7", verbose)
+    c.set_signal(88, "TOF18", "TOF module 1 channel 8", verbose)
+    c.set_signal(89, "TOF19", "TOF module 1 channel 9", verbose)
+    c.set_signal(90, "TOF1A", "TOF module 1 channel 10", verbose)
+    c.set_signal(91, "TOF1B", "TOF module 1 channel 11", verbose)
+    c.set_signal(92, "TOF1C", "TOF module 1 channel 12", verbose)
+    c.set_signal(93, "TOF1D", "TOF module 1 channel 13", verbose)
+    c.set_signal(94, "TOF1E", "TOF module 1 channel 14", verbose)
+    c.set_signal(95, "TDCT0", "TDC stop signal for TDC2", verbose)
+
+
+def setup_low_e_trigger_logic(c: Configuration, verbose = False):
+
+    c.set_level_1_logic(0, "T0 L1", "T0 coincidence", [0,1,2,3], "AND", verbose)
+    c.set_level_1_logic(1, "T1 L1", "T1 coincidence", [4,5,6,7], "AND", verbose)
+    c.set_level_1_logic(2, "ATCe", "ATC electron", [12,13], "OR", verbose)
+    c.set_level_1_treatment(2,invert=True, verbose=verbose)
+
+    c.set_level_2_logic(0, "LowE", "Low Energy Trigger with eVeto", [0,1,2], [], "AND", verbose)
+
+    return
+
+
+# Create a low energy trigger configuration object
+low_e = Configuration("Low E v1", "Low Energy Trigger - version 1.0")
+
+# Set signal inputs (as defined by the inputs to the CFD modules)
+set_signal_channels(low_e, verbose = True)
+
+# setup the trigger logic
+setup_low_e_trigger_logic(low_e, verbose = True)
+
+# direct the output of the trigger logic to lemo 0
+low_e.set_output_lemo_assignment(0, "Level 2", 0,False, verbose = True)
+
+xxx = 1
+
